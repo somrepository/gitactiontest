81726240e39d2de0b3caf5bb51fe39f4d7b6a8d7
SOMNATH VERMA <somverma.earth@outlook.com>
2025-04-27 09:04:13 -0400

    test

---
+++ provider.tf
@@ -0,0 +1,26 @@
+terraform {
+
+  backend "azurerm" {
+    subscription_id      = "2178b722-5baa-4418-8bc9-3fcb9cd14fed"
+    resource_group_name  = "gitaction_str"
+    storage_account_name = "masterstoragegithub"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "gitbackup"             # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "testgithub.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+
+
+}
+
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "2178b722-5baa-4418-8bc9-3fcb9cd14fed"
+
+}
---
+++ rg.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rg_name" {
+  name     = "jkl"
+  location = "eastus"
+
+}
